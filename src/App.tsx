7323f3357147f060437f98d6d35ce1ff3eea06b0
Srebrokryl <nikitakol2000@mail.ru>
2023-11-19 19:12:49 +0400

    Edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Card} from "./components/Card.styled";
+import {Header} from './components/Header.styled';
+import picture from "./images/Rectangle 1.svg"
+import {Headline} from "./components/Headline.styled";
+import {Faucibus} from "./components/Faucibus.styled";
+import {Photo} from './components/Photo.styled';
+import {Button1} from "./components/Button1.styled";
+import {Button2} from "./components/Button2.styled";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header>
+                <Card>
+                    <Photo src={picture} alt={"desert"}/>
+                    <Headline>Headline</Headline>
+                    <Faucibus>Faucibus. Faucibus. Sit sit sapien sit tempusrisu ut. Sit molestie ornare in
+                        venen.</Faucibus>
+                    <Button1>See more</Button1>
+                    <Button2>Save</Button2>
+                </Card>
+            </Header>
+        </div>
+    );
 }
 
 export default App;
