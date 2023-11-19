03ccc197458b75d35bd3f0eaa82974e19285a049
Srebrokryl <nikitakol2000@mail.ru>
2023-11-19 19:11:51 +0400

    Add Faucibus.styled.tsx

---
+++ src/components/Faucibus.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import {myTheme} from "../styles/Theme.styled";
+
+export const Faucibus = styled.p `
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 20px;
+  color: ${myTheme.colors.grey};
+  padding: 20px 20px 19px 20px;
+`
