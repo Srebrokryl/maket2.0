e6b4e16516bcdc23680fffdc27679512e44884b3
Srebrokryl <nikitakol2000@mail.ru>
2023-11-19 19:11:30 +0400

    Add Card.styled.tsx

---
+++ src/components/Card.styled.tsx
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Card = styled.div `
+  max-width: 300px;
+  min-height: 350px;
+  border-radius: 15px;
+  box-shadow: 0 4px 20px 5px rgba(0, 0, 0, 0.1);
+`
