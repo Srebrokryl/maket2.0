3bd0f05c69ee38ee8a6d4d10ed1b2491790a5c42
Srebrokryl <nikitakol2000@mail.ru>
2023-11-19 19:10:07 +0400

    Add Button1.styled.tsx and Button2.styled.tsx

---
+++ src/components/Button1.styled.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import {myTheme} from "../styles/Theme.styled";
+
+export const Button1 = styled.button `
+  width: 86px;
+  height: 30px;
+  border-radius: 5px;
+  border-color: ${myTheme.colors.blue};
+  background-color: ${myTheme.colors.blue};
+  color:${myTheme.colors.white};
+  margin-left: 20px;
+  margin-bottom: 22px;
+  //cursor: pointer;
+`
---
+++ src/components/Button2.styled.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import {myTheme} from "../styles/Theme.styled";
+
+export const Button2 = styled.button `
+  width: 86px;
+  height: 30px;
+  color: ${myTheme.colors.blue};
+  border-radius: 5px;
+  border-color: ${myTheme.colors.blue};
+  //cursor: pointer;
+  margin-left: 12px;
+`
